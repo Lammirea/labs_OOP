0a89ecd0e7f1f6078904a82e2620cf9048f6485b
Lammirea <71898618+Lammirea@users.noreply.github.com>
2022-02-16 15:54:02 +0300

    лабораторная 1

--- Лаб_1.cpp
+++ Лаб_1.cpp
@@ -1,7 +1,10 @@
 ﻿
+
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <locale.h>
+
 using namespace std;
 
 class Train
@@ -9,11 +12,17 @@ class Train
 private:
 	string Destination, time, type_of_train;
 	int num_of_train, num_of_seats;
+	int* p;
 public:
 	//Train(string destination, string time, string type_of_train, int num_of_train, int num_of_seats);
-	Train(string, string, string, int, int);
-	Train();
 	
+	Train();
+
+	string Set_Destination(string Dest) { Destination = Dest; }
+	string Set_Time(string Time) { time = Time; }
+	string Set_Type(string Type_of_train) { type_of_train = Type_of_train; }
+	int Set_Num(int Num_of_Train) { num_of_train = Num_of_Train; }
+	int Set_Seats(int Num_of_Seats) { num_of_seats = Num_of_Seats; }
 	string Get_Destination() { return Destination; }
 	string Get_Time() { return time; }
 	string Get_Type() { return type_of_train; }
@@ -22,32 +31,25 @@ public:
 	~Train();
 };
 
-Train::Train(string destination, string Time, string Type_of_train, int Num_of_Train, int Num_of_Seats)
-{
-	Destination = destination;
-	time = Time;
-	type_of_train = Type_of_train;
-	num_of_train = Num_of_Train;
-	num_of_seats = Num_of_Seats;
-}
 
 Train::Train()
 {
+	p = new int[100];
 }
 
-Train::~Train() {};
+Train::~Train() { delete p; };
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+	setlocale(LC_ALL, "Russian");
+	//SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
 	SetConsoleOutputCP(1251); // Вывод на консоль в кодировке 1251.
 	string Destination_, time_, type_of_train_;
-	int num_of_train_=0, num_of_seats_=0;
+	int num_of_train_ = 0, num_of_seats_ = 0;
 	int n;
 	cout << "Введите количество поездов:" << endl;
 	cin >> n;
-	Train *info=new Train[n];
+	Train* info = new Train[n];
 	for (int i = 0; i < n; i++)
 	{
 		cout << "Введите пункт достижения:" << endl;
@@ -61,11 +63,15 @@ int main()
 		cout << "Введите количество общих мест:" << endl;
 		cin >> num_of_seats_;
 
-		info[i] = Train(Destination_, time_, type_of_train_, num_of_train_, num_of_seats_);
+		info[i].Set_Destination(Destination_);
+		info[i].Set_Time(time_);
+		info[i].Set_Type(type_of_train_);
+		info[i].Set_Num(num_of_train_);
+		info[i].Set_Seats(num_of_seats_);
 	}
 	cout << "Выберите действия: " << endl << "1 Получить список поездов, следующих до заданного пункта назначения" << endl <<
 		"2 Получить список поездов, следующих до заданного пункта назначения и отправляющихся после заданного часа" << endl <<
-		"3 Получить список поездов, отправляющихся до заданного пункта назначения и имеющих общие места" << endl << 
+		"3 Получить список поездов, отправляющихся до заданного пункта назначения и имеющих общие места" << endl <<
 		"-----------------------------------------------------------------------------" << endl;;
 	int choice = 0;
 	cin >> choice;
@@ -75,19 +81,19 @@ int main()
 	case 1:
 		cout << "Введите пункт достижения:" << endl;
 		cin >> Destination_;
-		for (int i = 0; i < n; i++){
-		if (info[i].Get_Destination()==Destination_)
-		{
-			cout << info[i].Get_Num() << "\t" << info[i].Get_Time() << "\t" << info[i].Get_Seats() << "\t" << info[i].Get_Type() << endl;
-		}
-		//else cout << "Нет поездов, следующих до " << Destination_ << endl;
+		for (int i = 0; i < n; i++) {
+			if (info[i].Get_Destination() == Destination_)
+			{
+				cout << info[i].Get_Num() << "\t" << info[i].Get_Time() << "\t" << info[i].Get_Seats() << "\t" << info[i].Get_Type() << endl;
+			}
+			//else cout << "Нет поездов, следующих до " << Destination_ << endl;
 		}
 		break;
 	case 2:
 		cout << "Введите пункт назначения и время:" << endl;
 		cin >> Destination_ >> time_;
 		time_.replace(time_.find(ToReplace), ToReplace.length(), ReplaceWith);
-		for (int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			info[i].Get_Time().replace(info[i].Get_Time().find(ToReplace), ToReplace.length(), ReplaceWith);
 			if ((info[i].Get_Destination() == Destination_) && (atof(info[i].Get_Time().c_str()) > atof(time_.c_str())))
 			{
@@ -99,7 +105,7 @@ int main()
 	case 3:
 		cout << "Введите пункт назначения и общие места:" << endl;
 		cin >> Destination_ >> num_of_seats_;
-		for (int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			if ((info[i].Get_Destination() == Destination_) && (info[i].Get_Seats() == num_of_seats_))
 			{
 				cout << info[i].Get_Num() << "\t" << info[i].Get_Time() << "\t" << info[i].Get_Type() << endl;
@@ -111,6 +117,6 @@ int main()
 		break;
 	}
 
+	system("pause>null");
 	return 0;
 }
-
